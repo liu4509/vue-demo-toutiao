12c87f23aa4a5588bc6f4675489b25d9baf853fb
zhibin <2608606700@qq.com>
2022-06-26 12:13:21 +0800

    完成了打包发布

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require("path");
 
 // 自定义主题的文件路径
-const coverPath = path.join(__dirname, './src/cover.less')
+const coverPath = path.join(__dirname, "./src/cover.less");
 
 module.exports = {
   css: {
@@ -9,10 +9,26 @@ module.exports = {
       less: {
         lessOptions: {
           modifyVars: {
-            hack: `true; @import "${coverPath}";`
-          }
-        }
-      }
-    }
-  }
-}
+            hack: `true; @import "${coverPath}";`,
+          },
+        },
+      },
+    },
+  },
+  // 增强 vue-cli 的 webpack 配置项
+  configureWebpack: {
+    // 打包优化
+    externals: {
+      // import 时的包名称: window 全局的成员名称
+      "highlight.js": "hljs",
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      axios: "axios",
+      vant: "vant",
+      dayjs: "dayjs",
+      "socket.io-client": "io",
+      popmotion: "popmotion",
+    },
+  },
+};
